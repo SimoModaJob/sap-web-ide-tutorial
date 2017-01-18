5dda4275300b2bcabfe840afa673fac869adfb6d
Simone Modaelli <smodaelli@cutawaysolution.it>
2017-01-18 14:52:41 +0000

    Create tile block in main page

--- webapp/controller/main.controller.js
+++ webapp/controller/main.controller.js
@@ -4,6 +4,13 @@ sap.ui.define([
 	"use strict";
 
 	return Controller.extend("TestApp.controller.main", {
-
+		
+		onInit: function () {
+			// fare chiamata odata e impostare i parametri per le varie tile (get by id e set property)
+			console.log(this.getView().byId("tile1"));
+		},
+	    onAfterRendering: function () {
+	    }
+	    
 	});
 });
