2cce06046187a2238bb16bff93ab4c6c416d0fc5
Simone Modaelli <smodaelli@cutawaysolution.it>
2017-01-18 14:54:04 +0000

    fix error on controller

--- webapp/controller/main.controller.js
+++ webapp/controller/main.controller.js
@@ -7,7 +7,7 @@ sap.ui.define([
 		
 		onInit: function () {
 			// fare chiamata odata e impostare i parametri per le varie tile (get by id e set property)
-			console.log(this.getView().byId("tile1"));
+			// console.log(this.getView().byId("tile1"));
 		},
 	    onAfterRendering: function () {
 	    }
